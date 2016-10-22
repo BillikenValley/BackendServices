d230f95f53dccd31da84189d11ff1a8ad1d61cde
avikejriwal <avi.kejriwal@gmail.com>
2016-10-22 01:36:15 -0500

    fixed output stream; sorted

--- main.py
+++ main.py
@@ -5,30 +5,27 @@ import json
 def main(argv):
     user_objects = None
     shelter_objects = None
-    location = None
+    returnObj = {} #initialize everything to null
     try:
-        opts, args = getopt.getopt(argv,"hu:s:l:",["users=","shelters=", "location="])
+        opts, args = getopt.getopt(argv,"hu:s:l:",["users=","shelters="])
     except getopt.GetoptError:
-        print 'mainScript.py -u <userObjects> -s <shelterObjects> -l <location>'
+        print 'mainScript.py -u <userObjects> -s <shelterObjects>'
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':
-            print 'mainScript.py -u <userObjects> -s <shelterObjects> -l <location>'
+            print 'mainScript.py -u <userObjects> -s <shelterObjects>'
             sys.exit()
         elif opt in ("-u", "--users"):
             user_objects = arg
         elif opt in ("-s", "--shelters"):
             shelter_objects = arg
-        elif opt in ("-l", "--location"):
-            location = arg
     if user_objects and shelter_objects:
-        for user in user_objects:
-            for shelter in shelter_objects:
-                user_dict = json.loads(user)
-                shelter_dict = json.loads(shelter)
-                rank = computeRating(user_dict,shelter_dict)
-                returnObj = json.dumps({"user": user_dict, "shelter": shelter_dict, "rank": rank})
-                print returnObj #unicode?
+        for shelter in shelter_objects: #for each shelter in the list
+            users_dict = json.loads(user_objects)
+            shelter_dict = json.loads(shelter)
+            rank = computeRank(users_dict,shelter_dict)
+            returnObj['shelter by id'] = rank #amend this line later when data model is finalized
+        print sorted(returnObj, key=returnObj.__getitem__, reverse=True) #unicode?
 
 if __name__ == "__main__":
    main(sys.argv[1:])
